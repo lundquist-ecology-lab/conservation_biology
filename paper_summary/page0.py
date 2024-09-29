ed6e96d15918780126c3c87213c06ce1308046a6
Matthew Lundquist <mlundquist@mmm.edu>
2024-09-29 18:29:45 +0000

    image link added

--- paper_summary/page0.py
+++ paper_summary/page0.py
@@ -4,15 +4,15 @@ def page0_content():
     
     st.header("Paper Summary 1 Discussion")
     st.subheader("Aquatic insect functional diversity and nutrient content in urban streams in a medium-sized city")
-    
+    st.write("Lundquist, MJ and Zhu, W (2018)")
+    st.write("")
     st.write("Question 1: What are the major question(s) the author(s) are trying to address? How they relate to conservation biology topics?")
     st.write("")
     st.write("Question 2: What are the basic methods?")
     st.write("")
     st.write("Question 3: What are their data, including an analysis of at least one data figure?")
-    st.image("figure2.png")
+    st.image("https://github.com/lundquist-ecology-lab/conservation_biology/blob/main/paper_summary/figure2.png?raw=true")
     st.write("")
     st.write("Question 4: What are their conclusions?")
     st.write("")
     st.write("Question 5: What do you think the next steps should be if they were to continue to study?")
-    
